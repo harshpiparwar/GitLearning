42e8f04f319ca257e8efddc6272bdf88e169f005
ANIKET-PC <Harsh Piparwar>
2021-02-24 13:57:19 +0530

    Slight change

--- hello.java
+++ hello.java
@@ -1,4 +1,7 @@
 class Hello{
+    public static void fibonacci() {
+        
+    }
     public static calFactorial(x){}
     public static void name(String arg[]) {
         System.out.println("Hello_World")
